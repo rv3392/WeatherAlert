2765d10b703b5a1f22d5a7fc43529c96bbb2a9a9
rv3392 <richalverma00@gmail.com>
2019-10-10 19:34:10 +1000

    Added html template and template-based emails.

--- warning_sender.py
+++ warning_sender.py
@@ -1,19 +1,39 @@
-import requests
-from api_details import MAILGUN_API_KEY, MAILGUN_API_DOMAIN
-from email import Email
+from urllib import request
+from i_email import Email
 
 class WarningEmail(Email):
-    def __init__(self, to_email, subect, text):
-        pass
+    def __init__(self, to_email, content):
+        super().__init__(to_email)
+        self._create_email(content)
 
-    def create_email():
-        pass
+    def _create_email(self, content):
+        email_html = self._load_template()
+        state_lists = self._parse_content(content)
+        
+        for state in state_lists.keys():
+            print(state)
+            email_html = email_html.replace("{" + state + " List}", str(state_lists.get(state)))  
 
-    def load_template(self):
-        return requests.get(".\warning_email_template.html").content
+        self._html = email_html
+        self._text = "Email does not support html?"
+        self._subject = "Current Warnings for "
+
+    def _load_template(self):
+        return str(open("warning_email_template.html").read())
+
+    def _parse_content(self, content):
+        state_list_dictionary = {"Northern Territory":list(), \
+        "Queensland":list(), "NSW and ACT":list(), "Victoria":list(), \
+        "Tasmania":list(), "South Australia":list(), \
+        "Western Australia":list()}
+
+        for record in content:
+            state_list_dictionary.get(record[1]).append(record)
+
+        return state_list_dictionary
 
 def main():
-    email = Email("richalverma00@gmail.com", "Test", "Test")
+    email = WarningEmail("richalverma00@gmail.com", "Test")
     email.send()
 
 if __name__ == "__main__":
--- weather_alert.py
+++ weather_alert.py
@@ -1,16 +1,25 @@
 from warning_scraper import WarningScraper
 from pprint import pprint
+import sqlite3
+from warning_sender import WarningEmail
 
 def get_warnings():
     warning_scraper = WarningScraper()
     warning_list = warning_scraper.get_current_warnings()
     warning_scraper.write_to_db()
 
-def check_database_updates():
-    pass
+def send_email():
+    db = sqlite3.connect('warnings.db')
+    cursor = db.cursor()
+
+    email_content = cursor.execute('''SELECT * FROM warnings''').fetchall()
+
+    email = WarningEmail("richalverma00@gmail.com", email_content)
+    email.send()
 
 def main():
     get_warnings()
+    send_email()
 
 if __name__ == "__main__":
 	main()
